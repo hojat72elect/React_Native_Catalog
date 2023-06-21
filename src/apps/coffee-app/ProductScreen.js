ec70a8af8e6156230ffc274374d48c73d0e683e4
hojat <hojat72elect@gmail.com>
2023-06-21 19:09:42 -0400

    ProductScreen.js

--- src/apps/coffee-app/ProductScreen.js
+++ src/apps/coffee-app/ProductScreen.js
@@ -1,10 +1,29 @@
-import {View, Text} from 'react-native'
+import {View, Text, Image, TouchableOpacity} from 'react-native'
 import React from 'react'
+import {useNavigation} from "@react-navigation/native";
+import {StatusBar} from "expo-status-bar";
+import {ArrowLeftCircleIcon} from "react-native-heroicons/outline";
 
 export const ProductScreen = () => {
+
+    const navigation = useNavigation();
+
     return (
-        <View>
-            <Text>ProductScreen</Text>
+        <View className="flex-1">
+            <StatusBar style="light"/>
+            <Image
+                source={require('../../../assets/images/beansBackground2.png')}
+                style={{height: 300, borderBottomLeftRadius: 50, borderBottomRightRadius: 50}}
+                className="w-full absolute"
+            />
+            <View className="space-y-4">
+                <View className="mx-4 flex-row justify-between items-center">
+                    <TouchableOpacity>
+                        <ArrowLeftCircleIcon size={50} strokeWidth={1.2} color="white"/>
+                    </TouchableOpacity>
+                </View>
+            </View>
+
         </View>
     )
 }
