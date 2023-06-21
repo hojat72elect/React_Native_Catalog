fd8756827d6da35c911316246d759765ac5fd0c8
hojat <hojat72elect@gmail.com>
2023-06-20 21:57:07 -0400

    CoffeeCard.js

--- src/apps/coffee-app/HomeScreen.js
+++ src/apps/coffee-app/HomeScreen.js
@@ -1,50 +1,62 @@
-import {FlatList, Image, SafeAreaView, Text, TextInput, TouchableOpacity, View} from 'react-native'
+import {
+    Dimensions,
+    FlatList,
+    Image,
+    Platform,
+    SafeAreaView,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View
+} from 'react-native'
 import React, {useState} from 'react'
-import {StatusBar} from "expo-status-bar";
-import {MapPinIcon} from "react-native-heroicons/solid";
+import {StatusBar} from 'expo-status-bar';
+import Carousel from 'react-native-snap-carousel';
+import {BellIcon, MagnifyingGlassIcon} from 'react-native-heroicons/outline'
+import {MapPinIcon} from 'react-native-heroicons/solid'
 import {themeColors} from "../../common/CoffeeTheme";
-import {BellIcon, MagnifyingGlassIcon} from "react-native-heroicons/outline";
-import {categories} from "./Constants";
+import {categories, coffeeItems} from "./Constants";
+import {CoffeeCard} from "./CoffeeCard";
 
+const {width, height} = Dimensions.get('window');
+const ios = Platform.OS === 'ios';
 export const HomeScreen = () => {
-
     const [activeCategory, setActiveCategory] = useState(1);
 
     return (
         <View className="flex-1 relative bg-white">
             <StatusBar/>
+
             <Image
                 source={require('../../../assets/images/beansBackground1.png')}
-                className="w-full absolute -top-5 opacity-10"
-                style={{height: 220}}
-            />
-            <SafeAreaView className="flex-1 mt-10">
+                style={{height: height * 0.2}}
+                className="w-full absolute -top-5 opacity-10"/>
+            <SafeAreaView className={ios ? '-mb-8' : ''}>
+                {/* avatar and bell icon */}
+                <View className="mx-4 flex-row justify-between items-center">
+                    <Image source={require('../../../assets/images/avatar.png')}
+                           className="h-9 w-9 rounded-full"/>
 
-                {/* Avatar and bell icon. */}
-                <View className="px-4 flex-row justify-between items-center">
-                    <Image
-                        source={require('../../../assets/images/avatar.png')}
-                        className="h-9 w-9 rounded-full"
-                    />
                     <View className="flex-row items-center space-x-2">
                         <MapPinIcon size="25" color={themeColors.bgLight}/>
-                        <Text className="text-base font-semibold">Toronto, Canada</Text>
+                        <Text className="font-semibold text-base">
+                            New York, NYC
+                        </Text>
                     </View>
                     <BellIcon size="27" color="black"/>
                 </View>
-                {/*search bar*/}
-                <View className="mx-5 mt-14">
-                    <View className="flex-row justify-center items-center rounded-full p-1 bg-[#e6e6e6]">
-                        <TextInput placeholder="Search" className="p-4 flex-1 font-semibold text-gray-700"/>
+                {/* search bar */}
+                <View className="mx-5 shadow" style={{marginTop: height * 0.06}}>
+                    <View className="flex-row items-center rounded-full p-1 bg-[#e6e6e6]">
+                        <TextInput placeholder='Search' className="p-4 flex-1 font-semibold text-gray-700"/>
                         <TouchableOpacity
                             className="rounded-full p-2"
-                            style={{backgroundColor: themeColors.bgLight}}
-                        >
-                            <MagnifyingGlassIcon size={25} strokeWidth={2} color="white"/>
+                            style={{backgroundColor: themeColors.bgLight}}>
+                            <MagnifyingGlassIcon size="25" strokeWidth={2} color="white"/>
                         </TouchableOpacity>
                     </View>
                 </View>
-                {/*Categories*/}
+                {/* categories */}
                 <View className="px-5 mt-6">
                     <FlatList
                         horizontal
@@ -58,23 +70,36 @@ export const HomeScreen = () => {
                             return (
                                 <TouchableOpacity
                                     onPress={() => setActiveCategory(item.id)}
-                                    style={{backgroundColor: isActive ? themeColors.bgLight : 'rgba(0, 0, 0, 0.07)'}}
-                                    className="py-4 px-5 rounded-full mr-2 shadow"
-                                >
-                                    <Text className={`font-semibold ${activeTextClass}`}>
-                                        {item.title}
-                                    </Text>
+                                    style={{backgroundColor: isActive ? themeColors.bgLight : 'rgba(0,0,0,0.07)'}}
+                                    className="p-4 px-5 mr-2 rounded-full shadow">
+                                    <Text className={"font-semibold " + activeTextClass}>{item.title}</Text>
                                 </TouchableOpacity>
                             )
                         }}
                     />
                 </View>
 
-                {/*Coffee cards*/}
-
             </SafeAreaView>
+
+            {/* coffee cards */}
+            <View className={`overflow-visible flex justify-center flex-1 ${ios ? 'mt-10' : ''}`}>
+                <View>
+                    <Carousel
+                        containerCustomStyle={{overflow: 'visible'}}
+                        data={coffeeItems}
+                        renderItem={({item}) => <CoffeeCard item={item}/>}
+                        firstItem={1}
+                        inactiveSlideScale={0.75}
+                        inactiveSlideOpacity={0.75}
+                        sliderWidth={width}
+                        itemWidth={width * 0.63}
+                        slideStyle={{display: 'flex', alignItems: 'center'}}
+                    />
+                </View>
+
+            </View>
+
+
         </View>
     )
 }
-
-
