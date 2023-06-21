374f4a15132c60ddd418b91b4c0d983fe6e73c68
hojat <hojat72elect@gmail.com>
2023-06-21 14:11:50 -0400

    Installed @react-navigation/bottom-tabs and added a bottom tab bar

--- src/apps/coffee-app/CoffeeAppNavigation.js
+++ src/apps/coffee-app/CoffeeAppNavigation.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
-import {LogBox} from "react-native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {LogBox, View} from "react-native";
 import {HomeScreen} from "./HomeScreen";
-
+import {themeColors} from "../../common/CoffeeTheme";
+import {
+    HomeIcon as HomeOutline,
+    HeartIcon as HeartOutline,
+    ShoppingBagIcon as ShoppingBagOutline
+} from "react-native-heroicons/outline";
+import {
+    HomeIcon as HomeSolid,
+    HeartIcon as HeartSolid,
+    ShoppingBagIcon as ShoppingBagSolid
+} from "react-native-heroicons/solid";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
 
 LogBox.ignoreLogs([
     'Non-serializable values were found in the navigation state',
@@ -16,10 +29,58 @@ LogBox.ignoreLogs([
  */
 export const CoffeeAppNavigation = () => {
 
+
+    const menuIcons = (route, focused) => {
+        let icon;
+        if (route.name === 'home') {
+            icon = focused ? <HomeSolid size={30} color={themeColors.bgLight}/> :
+                <HomeOutline size={30} strokeWidth={2} color="white"/>
+        } else if (route.name === 'favorite') {
+            icon = focused ? <HeartSolid size={30} color={themeColors.bgLight}/> :
+                <HeartOutline size={30} strokeWidth={2} color="white"/>
+        } else if (route.name === 'cart') {
+            icon = focused ? <ShoppingBagSolid size={30} color={themeColors.bgLight}/> :
+                <ShoppingBagOutline size={30} strokeWidth={2} color="white"/>
+        }
+        let buttonClass = focused ? 'bg-white' : '';
+
+        return (
+            <View className={`flex items-center rounded-full p-3 shadow ${buttonClass}`}>
+                {icon}
+            </View>
+        )
+    }
+
+    const HomeTabs = () => {
+        return (
+            <Tab.Navigator
+                screenOptions={({route}) => ({
+                    headerShown: false,
+                    tabBarShowLabel: false,
+                    tabBarIcon: ({focused, color, size}) => menuIcons(route, focused),
+                    tabBarStyle: {
+                        height:60,
+                        marginBottom: 10,
+                        borderRadius: 50,
+                        backgroundColor: themeColors.bgLight,
+                        marginHorizontal: 20,
+                        position:'absolute'
+                    }
+                })}
+            >
+                <Tab.Screen name="home" component={HomeScreen}/>
+                <Tab.Screen name="favorite" component={HomeScreen}/>
+                <Tab.Screen name="cart" component={HomeScreen}/>
+            </Tab.Navigator>
+        )
+    }
+
     return (
         <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name="Home" options={{headerShown: false}} component={HomeScreen}/>
+            <Stack.Navigator screenOptions={{
+                contentStyle: {backgroundColor: 'white'}
+            }}>
+                <Stack.Screen name="Home" options={{headerShown: false}} component={HomeTabs}/>
             </Stack.Navigator>
         </NavigationContainer>
     );
