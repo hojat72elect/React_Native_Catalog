d8d3d2d6fc990788179b7255b8e28d15107ca537
hojat <hojat72elect@gmail.com>
2023-06-21 22:26:55 -0400

    ProductScreen.js

--- src/apps/coffee-app/ProductScreen.js
+++ src/apps/coffee-app/ProductScreen.js
@@ -1,14 +1,16 @@
 import {View, Text, Image, TouchableOpacity} from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 import {useNavigation} from "@react-navigation/native";
 import {StatusBar} from "expo-status-bar";
 import {ArrowLeftCircleIcon} from "react-native-heroicons/outline";
-import {HeartIcon} from "react-native-heroicons/solid";
+import {HeartIcon, StarIcon} from "react-native-heroicons/solid";
+import {themeColors} from "../../common/CoffeeTheme";
 
 export const ProductScreen = (props) => {
 
     const item = props.route.params;
     const navigation = useNavigation();
+    const [orderSize, setOrderSize] = useState('small');
 
     return (
         <View className="flex-1">
@@ -20,19 +22,66 @@ export const ProductScreen = (props) => {
             />
             <View className="space-y-4 mt-6">
                 <View className="mx-4 flex-row justify-between items-center">
-                    <TouchableOpacity onPress={()=>navigation.goBack()}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
                         <ArrowLeftCircleIcon size={50} strokeWidth={1.2} color="white"/>
                     </TouchableOpacity>
 
                     <TouchableOpacity className="rounded-full border-2 border-white p-2">
-                        <HeartIcon size={24}  color="white"/>
+                        <HeartIcon size={24} color="white"/>
                     </TouchableOpacity>
                 </View>
-                <View>
-                    <Image source={item.image} className=""/>
+                <View
+                    className="flex-row justify-center"
+                    style={{
+                        shadowColor: themeColors.bgDark,
+                        shadowRadius: 30,
+                        shadowOffset: {width: 0, height: 30},
+                        shadowOpacity: 0.9
+                    }}
+                >
+                    <Image source={item.image} className="h-60 w-60"/>
+                </View>
+                <View style={{backgroundColor: themeColors.bgLight}}
+                      className="flex-row mx-4 items-center rounded-3xl p-1 px-2 space-x-1 w-16 opacity-90">
+                    <StarIcon size="15" color="white"/>
+                    <Text className="text-base font-semibold text-white">{item.stars}</Text>
+                </View>
+                <View className="mx-4 flex-row justify-between items-center">
+                    <Text style={{color: themeColors.text}} className="text-3xl font-semibold">{item.name}</Text>
+                    <Text style={{color: themeColors.text}} className="text-lg font-semibold">$ {item.price}</Text>
+                </View>
+                <View className="mx-4 space-y-2">
+                    <Text style={{color: themeColors.text}} className="text-lg font-bold">
+                        Coffee size
+                    </Text>
+                    <View className="flex-row justify-between">
+                        <TouchableOpacity
+                            onPress={() => setOrderSize('small')}
+                            className="px-8 py-3 rounded-full"
+                            style={{backgroundColor: orderSize === 'small' ? themeColors.bgLight : 'rgba(0, 0, 0, 0.07)'}}
+                        >
+                            <Text className={orderSize === 'small' ? "text-white" : "text-gray-700"}>Small</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => setOrderSize('medium')}
+                            className="px-8 py-3 rounded-full"
+                            style={{backgroundColor: orderSize === 'medium' ? themeColors.bgLight : 'rgba(0, 0, 0, 0.07)'}}
+                        >
+                            <Text className={orderSize === 'medium' ? "text-white" : "text-gray-700"}>Medium</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            onPress={() => setOrderSize('large')}
+                            className="px-8 py-3 rounded-full"
+                            style={{backgroundColor: orderSize === 'large' ? themeColors.bgLight : 'rgba(0, 0, 0, 0.07)'}}
+                        >
+                            <Text className={orderSize === 'large' ? "text-white" : "text-gray-700"}>Large</Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
             </View>
-
         </View>
     )
 }
