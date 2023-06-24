c55e3d5c1ac65991d3741a8be25ab120984703dc
hojat <hojat72elect@gmail.com>
2023-06-24 13:58:35 -0400

    initial implementation of step counter completed

--- src/apps/step-counter-app/StepCounterAppNavigation.tsx
+++ src/apps/step-counter-app/StepCounterAppNavigation.tsx
@@ -1,7 +1,10 @@
-import {View, StyleSheet} from 'react-native'
+import {View, StyleSheet, Text} from 'react-native'
 import React, {useState} from 'react'
 import {useHealthData} from "./hooks/useHealthData";
 import {AntDesign} from '@expo/vector-icons';
+import {RingProgress} from "./components/RingProgress";
+import {Value} from "./components/Value";
+import {StatusBar} from "expo-status-bar";
 
 const STEPS_GOAL: number = 10_000;
 
@@ -48,14 +51,31 @@ export default function StepCounterAppNavigation() {
         <View style={styles.container}>
             <View style={styles.datePicker}>
                 <AntDesign
+                    onPress={() => changeDate(-1)}
+                    name="right"
+                    size={20}
+                    color="#C3FF53"
+                />
+                <Text style={styles.date}>{date.toDateString()}</Text>
+
+                <AntDesign
                     onPress={() => changeDate(1)}
                     name="right"
                     size={20}
                     color="#C3FF53"
                 />
             </View>
-
-
+            <RingProgress
+                radius={150}
+                strokeWidth={50}
+                progress={steps / STEPS_GOAL}
+            />
+            <View style={styles.values}>
+                <Value label="Steps" value={steps.toString()}/>
+                <Value label="Distance" value={`${(distance / 1000).toFixed(2)} km`}/>
+                <Value label="Flights Climbed" value={flights.toString()}/>
+            </View>
+            <StatusBar style="auto"/>
 
         </View>
     );
---
+++ src/apps/step-counter-app/components/Value.tsx
@@ -0,0 +1,25 @@
+import {Text, View, StyleSheet} from "react-native";
+
+type ValueProps = {
+    label: string;
+    value: string;
+};
+
+const styles = StyleSheet.create({
+    label: {
+        color: 'white',
+        fontSize: 20,
+    },
+    value: {
+        fontSize: 45,
+        color: '#AFB3BE',
+        fontWeight: '500',
+    },
+});
+
+export const Value = ({label, value}: ValueProps) => (
+    <View>
+        <Text style={styles.label}>{label}</Text>
+        <Text style={styles.value}>{value}</Text>
+    </View>
+);
