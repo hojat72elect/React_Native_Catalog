c17eaf20947c3612d147f8913310b1117eba04ac
hojat <hojat72elect@gmail.com>
2023-06-25 20:29:46 -0400

    created RingProgress.tsx

--- src/apps/step-counter-app/StepCounterAppNavigation.tsx
+++ src/apps/step-counter-app/StepCounterAppNavigation.tsx
@@ -2,6 +2,7 @@ import {View, StyleSheet} from 'react-native'
 import React from 'react'
 import {StatusBar} from "expo-status-bar";
 import {Value} from "./Value";
+import RingProgress from "./RingProgress";
 
 const styles = StyleSheet.create({
     container: {
@@ -21,12 +22,13 @@ export default function StepCounterAppNavigation() {
     return (
         <View style={styles.container}>
 
+            <RingProgress/>
+
             <View style={styles.valuesRow}>
                 <Value label="Steps" value="1219"/>
                 <Value label="Distance" value="0,75 Km"/>
                 <Value label="Flights Climbed" value="12"/>
             </View>
-
 
             <StatusBar style="auto"/>
         </View>
