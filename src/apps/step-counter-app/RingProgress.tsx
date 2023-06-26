5f78b0b3ff56b21379b76c857a9f0f26560b0fb0
hojat <hojat72elect@gmail.com>
2023-06-25 23:13:57 -0400

    implemented RingProgress.tsx

--- src/apps/step-counter-app/RingProgress.tsx
+++ src/apps/step-counter-app/RingProgress.tsx
@@ -4,20 +4,47 @@ import SVG, {Circle} from "react-native-svg";
 
 type RingProgressProps = {
     radius?: number;
+    strokeWidth?: number;
+    progress: number;
 };
 
 const color = "#EE0F55";
 
-export default function RingProgress({radius = 50}: RingProgressProps) {
+export default function RingProgress({radius = 90, strokeWidth = 18, progress}: RingProgressProps) {
+
+    const innerRadius = radius - strokeWidth / 2;
+    const circumference = 2 * Math.PI * innerRadius;
+
     return (
         <View style={{
             width: radius * 2,
             height: radius * 2,
-            alignSelf: 'center'
+            alignSelf: 'center',
         }}>
 
             <SVG>
-                <Circle r={radius} fill={color}/>
+                {/*Background circle*/}
+                <Circle
+                    cx={radius}
+                    cy={radius}
+                    r={innerRadius}
+                    strokeWidth={strokeWidth}
+                    stroke={color}
+                    opacity={0.2}
+                />
+                {/*Foreground circle*/}
+                <Circle
+                    cx={radius}
+                    cy={radius}
+                    r={innerRadius}
+                    strokeWidth={strokeWidth}
+                    stroke={color}
+                    strokeDasharray={[circumference * progress, circumference]}
+                    strokeLinecap='round'
+                    rotation="-90"
+                    originX={radius}
+                    originY={radius}
+                />
             </SVG>
         </View>
     );
--- src/apps/step-counter-app/StepCounterAppNavigation.tsx
+++ src/apps/step-counter-app/StepCounterAppNavigation.tsx
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         gap: 50,
         flexWrap: 'wrap',
+        marginTop: 50,
     }
 });
 
@@ -22,7 +23,7 @@ export default function StepCounterAppNavigation() {
     return (
         <View style={styles.container}>
 
-            <RingProgress/>
+            <RingProgress progress={0.95}/>
 
             <View style={styles.valuesRow}>
                 <Value label="Steps" value="1219"/>
