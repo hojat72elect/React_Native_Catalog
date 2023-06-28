ad93fcc4bee37a6b0f321b5f866808b66d5009ad
hojat <hojat72elect@gmail.com>
2023-06-28 11:13:32 -0400

    rolled back moti tutorial

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import StepCounterAppNavigation from "./src/apps/step-counter-app/StepCounterAppNavigation";
+import 'react-native-reanimated';
+import 'react-native-gesture-handler';
+import MotiTutorialAppNavigation from "./src/apps/moti-tutorial-app/MotiTutorialAppNavigation";
 
 export default function App() {
     return (
-        <StepCounterAppNavigation/>
+        <MotiTutorialAppNavigation/>
     );
 }
