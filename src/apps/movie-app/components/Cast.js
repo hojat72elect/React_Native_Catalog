6b965b02d97b8a98d3ca25734767fc5cabe19565
hojat <hojat72elect@gmail.com>
2023-06-28 21:25:01 -0400

    hooked up cast and person screen with API layer

--- src/apps/movie-app/components/Cast.js
+++ src/apps/movie-app/components/Cast.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import {Image, ScrollView, Text, TouchableOpacity, View} from "react-native";
-import {useNavigation} from "@react-navigation/native";
+import {fallbackPersonImage, image185} from "../api/MovieDb";
 
-export const Cast = ({cast}) => {
+export const Cast = ({cast, navigation}) => {
 
-    const personName = "Hojat Ghasemi"
-    const characterName = "Kaveh Ahangar"
-    const navigation = useNavigation();
 
     return (
         <View className="my-6">
@@ -16,31 +13,39 @@ export const Cast = ({cast}) => {
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{paddingHorizontal: 15}}
             >
-                {cast && cast.map((person, index) => {
-                    return (
-                        <TouchableOpacity
-                            key={index}
-                            className="mr-4 items-center"
-                            onPress={() => {
-                                console.log("user clicked on an actor's pic");
-                                navigation.navigate('Person');
-                            }}
-                        >
-                            <View
-                                className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
-                                <Image className="rounded-2xl h-24 w-20"
-                                       source={require('../../../../assets/images/castImage1.png')}/>
-                            </View>
-                            <Text className="text-white text-xs mt-1">
-                                {characterName.length > 10 ? characterName.slice(0, 10) + '...' : characterName}
-                            </Text>
-                            <Text className="text-neutral-400 text-xs mt-1">
-                                {personName.length > 10 ? personName.slice(0, 10) + '...' : personName}
-                            </Text>
-                        </TouchableOpacity>
-                    );
-                })}
+                {
+                    cast && cast.map((person, index)=>{
+                        return (
+                            <TouchableOpacity
+                                key={index}
+                                onPress={()=> navigation.navigate('Person', person)}
+                                className="mr-4 items-center">
+                                <View
+                                    className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
+                                    <Image
+                                        className="rounded-2xl h-24 w-20"
+                                        // source={require('../assets/images/castImage1.png')}
+                                        source={{uri: image185(person?.profile_path) || fallbackPersonImage}}
+                                    />
+                                </View>
+
+                                <Text className="text-white text-xs mt-1">
+                                    {
+                                        person?.character.length>10? person.character.slice(0,10)+'...' : person?.character
+                                    }
+                                </Text>
+                                <Text className="text-neutral-400 text-xs">
+                                    {
+                                        person?.original_name.length>10? person.original_name.slice(0,10)+'...' : person?.original_name
+                                    }
+                                </Text>
+                            </TouchableOpacity>
+                        )
+                    })
+                }
+
             </ScrollView>
+
         </View>
     )
 }
