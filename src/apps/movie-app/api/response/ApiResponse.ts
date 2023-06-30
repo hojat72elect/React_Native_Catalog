28d765d08411a8abc9c4fcc1b2c1d5abbc1c8813
hojat <hojat72elect@gmail.com>
2023-06-30 16:37:51 -0400

    defined further data types for cast API response

--- src/apps/movie-app/api/response/ApiResponse.ts
+++ src/apps/movie-app/api/response/ApiResponse.ts
@@ -4,7 +4,7 @@ export type ApiResponse = {
     results: ApiResponseResults[];
     total_pages: number;
     total_results: number;
-}
+};
 
 export type ApiResponseResults = {
     adult: boolean;
@@ -22,4 +22,39 @@ export type ApiResponseResults = {
     video: boolean;
     vote_average: number;
     vote_count: number;
-}
+};
+
+export type ApiMovieCredits = {
+    cast: ApiMovieCreditsCast[];
+    crew: ApiMovieCreditsCrew[];
+    id: number;
+};
+
+export type ApiMovieCreditsCast = {
+    adult: boolean;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    order: number;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+};
+
+export type ApiMovieCreditsCrew = {
+    adult: boolean;
+    credit_id: string;
+    department: string;
+    gender: number;
+    id: number;
+    job: string;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+};
