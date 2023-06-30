aa1d79fe29dbf2c555c3bfe1b36876c0c2356851
hojat <hojat72elect@gmail.com>
2023-06-30 14:24:54 -0400

    converted API result to types

---
+++ src/apps/movie-app/api/response/ApiResponse.ts
@@ -0,0 +1,23 @@
+export type ApiResponse = {
+    page: number;
+    results: ApiResponseResults[];
+    total_pages: number;
+    total_results: number;
+}
+export type ApiResponseResults = {
+    adult: boolean;
+    backdrop_path: string;
+    genre_ids: string[];
+    id: number;
+    media_type: string;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string;
+    release_date: string;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
