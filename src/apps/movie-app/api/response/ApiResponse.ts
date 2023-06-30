5f265c746f3258a2d2fb137541d451aaa9c1f917
hojat <hojat72elect@gmail.com>
2023-06-30 18:12:29 -0400

    Cleaned

--- src/apps/movie-app/api/response/ApiResponse.ts
+++ src/apps/movie-app/api/response/ApiResponse.ts
@@ -23,38 +23,3 @@ export type ApiResponseResults = {
     vote_average: number;
     vote_count: number;
 };
-
-export type ApiMovieCredits = {
-    cast: ApiMovieCreditsCast[];
-    crew: ApiMovieCreditsCrew[];
-    id: number;
-};
-
-export type ApiMovieCreditsCast = {
-    adult: boolean;
-    cast_id: number;
-    character: string;
-    credit_id: string;
-    gender: number;
-    id: number;
-    known_for_department: string;
-    name: string;
-    order: number;
-    original_name: string;
-    popularity: number;
-    profile_path: string;
-};
-
-export type ApiMovieCreditsCrew = {
-    adult: boolean;
-    credit_id: string;
-    department: string;
-    gender: number;
-    id: number;
-    job: string;
-    known_for_department: string;
-    name: string;
-    original_name: string;
-    popularity: number;
-    profile_path: string;
-};
