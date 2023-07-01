064d43e6d3f1defdfc97223c8e266a0b91975e80
hojat <hojat72elect@gmail.com>
2023-07-01 15:02:39 -0400

    App.js -> TSX

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 /// <reference types="nativewind/types" />
 import {CoffeeAppNavigation} from "./src/apps/coffee-app/CoffeeAppNavigation";
+import React from "react";
 
 export default function App() {
     return (
