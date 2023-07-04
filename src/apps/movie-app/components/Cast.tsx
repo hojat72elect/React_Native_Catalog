e2fb46070868ee654d524d045045ee33dae9adb9
hojat <hojat72elect@gmail.com>
2023-07-04 16:08:36 -0400

    Changed styling in Cast.tsx

--- src/apps/movie-app/components/Cast.tsx
+++ src/apps/movie-app/components/Cast.tsx
@@ -13,12 +13,18 @@ type CastProps = {
 export const Cast = ({cast, navigation}: CastProps) => {
 
     return (
-        <View className="my-6">
-            <Text className="text-white text-lg mx-4 mb-5">Top Cast</Text>
+        <View style={{marginVertical: 8}}>
+            <Text
+                style={{
+                    color: 'white',
+                    fontSize: 18,
+                    lineHeight: 24,
+                    marginHorizontal: 8
+                }}>Top Cast</Text>
             <ScrollView
                 horizontal
                 showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{paddingHorizontal: 15}}
+                style={{paddingHorizontal: 15, marginTop: 10}}
             >
                 {
                     cast && cast.map((person, index) => {
@@ -27,21 +33,27 @@ export const Cast = ({cast, navigation}: CastProps) => {
                                 key={index}
                                 // @ts-ignore
                                 onPress={() => navigation.navigate('Person', person)}
-                                className="mr-4 items-center">
-                                <View
-                                    className="overflow-hidden rounded-full h-20 w-20 items-center border border-neutral-500">
-                                    <Image
-                                        className="rounded-2xl h-24 w-20"
-                                        // source={require('../assets/images/castImage1.png')}
-                                        source={{uri: image185(person?.profile_path) || fallbackPersonImage}}
-                                    />
-                                </View>
+                                style={{marginRight: 8, flexDirection: 'column', alignItems: 'center'}}
+                            >
+
+                                <Image
+                                    style={{
+                                        overflow: 'hidden',
+                                        borderRadius: 1000,
+                                        height: 72,
+                                        width: 72,
+                                        borderWidth: 2,
+                                        borderColor: 'rgb(115 115 115)',
+                                    }}
+                                    source={{uri: image185(person?.profile_path) || fallbackPersonImage}}
+                                />
 
                                 <Text className="text-white text-xs mt-1">
                                     {
                                         person?.character.length > 10 ? person.character.slice(0, 10) + '...' : person?.character
                                     }
                                 </Text>
+
                                 <Text className="text-neutral-400 text-xs">
                                     {
                                         person?.original_name.length > 10 ? person.original_name.slice(0, 10) + '...' : person?.original_name
