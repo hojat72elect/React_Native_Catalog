a5e2a3ecafeeef6f2bd6d24e1dbe938e597dabd9
hojat <hojat72elect@gmail.com>
2023-07-14 16:09:22 -0400

    created bird game object

--- src/apps/flappy-bird-game/FlappyBirdGameNavigation.jsx
+++ src/apps/flappy-bird-game/FlappyBirdGameNavigation.jsx
@@ -3,6 +3,7 @@ import {View} from "react-native";
 import {GameEngine} from "react-native-game-engine";
 import Matter from 'matter-js';
 import {Constants} from "./Constants";
+import {Bird} from "./Bird";
 
 
 export class FlappyBirdGameNavigation extends Component {
@@ -36,9 +37,19 @@ export class FlappyBirdGameNavigation extends Component {
      * sets up the matter JS which is our physics engine.
      */
     setupWorld() {
+        // the physics engine
         const engine = Matter.Engine.create({enableSleeping: false});
+        // A Matter.Composite instance that holds all the physical entities in our world.
         const world = engine.world;
         const bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+        console.log("type of bird is :", typeof bird);
+        // Add the bird to our world
+        Matter.Composite.add(world, [bird]);
+
+        return {
+            physics: {engine: engine, world: world},
+            bird: {body: bird, size: [50, 50], color: 'red', renderer: Bird}
+        }
     }
 }
 
